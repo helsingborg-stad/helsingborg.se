c9738d79938c2aa5db0cc6b89a9a4a0f351718c5
nRamstedt <nikolas.ramstedt@gmail.com>
2021-05-18 11:29:27 +0200

    add example env files

---
+++ config/example.database.php
@@ -0,0 +1,21 @@
+<?php
+
+/** The name of the database for WordPress */
+define('DB_NAME', 'helsingborg_test');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', '');
+
+/** MySQL hostname */
+define('DB_HOST', '127.0.0.1');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8mb4');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+$table_prefix = 'hbg_';
