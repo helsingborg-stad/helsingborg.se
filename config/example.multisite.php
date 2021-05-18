d55fcfb9be49fc4483fdd3af69df06f92384632a
nRamstedt <nikolas.ramstedt@gmail.com>
2021-05-18 11:29:27 +0200

    add example env files

---
+++ config/example.multisite.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Tell WordPress to be used as network
+ */
+define('WP_ALLOW_MULTISITE', true);
+define('MULTISITE', true);
+define('SUBDOMAIN_INSTALL', true);
+define('DOMAIN_CURRENT_SITE', 'helsingborg.test');
+define('PATH_CURRENT_SITE', '/');
+define('SITE_ID_CURRENT_SITE', 1);
+define('BLOG_ID_CURRENT_SITE', 1);
